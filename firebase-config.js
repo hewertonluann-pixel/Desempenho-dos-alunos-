fc6935da05c8b9011fa6710bf8292845fe1dc476
hewertonluann-pixel <hewertonluann@gmail.com>
2026-01-26 06:17:22 -0300

    Update storageBucket URL in firebase config

--- firebase-config.js
+++ firebase-config.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDdMROcKph5I-ClMiOmPiBXgGpDxoF2dZc",
   authDomain: "asafenotas-5cf3f.firebaseapp.com",
   projectId: "asafenotas-5cf3f",
-  storageBucket: "asafenotas-5cf3f.appspot.com",
+  storageBucket: "asafenotas-5cf3f.firebasestorage.app",
   messagingSenderId: "312062581585",
   appId: "1:312062581585:web:432ff63a527dd86fc1170",
   measurementId: "G-Z6G6D4RKZQ"
